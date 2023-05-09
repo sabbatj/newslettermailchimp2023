bc4a8287ef92ca2531b3cb95c9b0e0338a58a4b0
kaye <kayeburgers@gmail.com>
2023-05-10 00:04:24 +0200

    this is a login form api

---
+++ README.md
@@ -0,0 +1,19 @@
+# MailChimp Newsletter Signup
+
+ Newsletter signup using Node.js/Express and the Mailchimp API. 
+ 
+ Please fill in your [audience ID] and your [API Key] in app.js
+
+ 
+# Install dependencies
+npm install
+ 
+ # Serve on localhost:3000
+npm start
+npm run dev (Nodemon)
+
+## App Info
+Juandre Sabbat .
+
+### License
+Project is not licensed with MIT.
---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const fetch = require('node-fetch');
+require('dotenv').config()
+
+const app = express();
+
+
+
+
+
+// Bodyparser Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Signup Route
+app.post('/signup', (req, res) => {
+  const { firstName, lastName, email } = req.body;
+
+  // Make sure fields are filled
+  if (!firstName || !lastName || !email) {
+    res.redirect('/fail.html');
+    return;
+  }
+
+  // Construct req data
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: 'subscribed',
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const postData = JSON.stringify(data);
+
+  fetch('https://us21.api.mailchimp.com/3.0/lists/'+ process.env.UNIQUE_ID, {
+    method: 'POST',
+    headers: {
+      Authorization:"kaye123" + process.env.API_KEY, 
+    },
+    body: postData
+  })
+    .then(res.statusCode === 200 ?
+      res.redirect('/success.html') :
+      res.redirect('/fail.html'))
+    .catch(err => console.log(err))
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server started on ${PORT}`));
+//<YOUR_AUDIENCE_ID>//
+//'auth <YOUR_API_KEY>'//
